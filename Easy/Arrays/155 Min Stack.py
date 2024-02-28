c00788f3dcdd7412af6b60c5d7ebe2de065b4027
debarghya-choudhury <debarghya.choudhury@yahoo.com>
2024-02-28 11:27:24 +0530

    solved 155 but O(n) later need to comeback and solve in O(1)

---
+++ Easy/Arrays/155 Min Stack.py
@@ -0,0 +1,18 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)   # O(n)
+    
+# Will later comeback to this problem and do the O(1) solution

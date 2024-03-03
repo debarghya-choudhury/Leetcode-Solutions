726f9ff5f276e2b2fb1759ad0c9517442ddb924d
debarghya-choudhury <debarghya.choudhury@yahoo.com>
2024-03-03 14:24:06 +0530

    solved 206 and 509 using recursion

--- Easy/Linked Lists/206. Reverse Linked List.py
+++ Easy/Linked Lists/206. Reverse Linked List.py
@@ -5,10 +5,32 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr = None
-        while head:
-            hold = head.next
-            head.next = curr
-            curr = head
-            head = hold 
-        return curr    
+        # curr = None
+        # while head:
+        #     hold = head.next
+        #     head.next = curr
+        #     curr = head
+        #     head = hold 
+        # return curr       
+
+        # Two pointers
+        # prev, curr = None, head
+        # while curr:
+        #     hold = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = hold
+        # return prev
+
+        # recursive approach
+        if not head:
+            return None
+
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head 
+        head.next = None
+        return newHead
+      
+
+

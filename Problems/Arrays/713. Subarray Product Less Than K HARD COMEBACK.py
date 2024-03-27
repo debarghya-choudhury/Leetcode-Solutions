0c994279adbe9aedc6d0b5514997fbf7ab6820a2
debarghya-choudhury <debarghya.choudhury@yahoo.com>
2024-03-27 18:51:11 +0530

    kinda solved 713 need to recheck

---
+++ Problems/Arrays/713. Subarray Product Less Than K HARD COMEBACK.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # basically if a subarray multiplication is < k then all of its internal subarray multiplications also will be < k
+        res = 0 
+        l = 0
+        product = 1
+        for r in range(len(nums)):
+            product *= nums[r]
+            while l <= r and product >= k:
+                product = product // nums[l]
+                l += 1
+            res += (r - l + 1)
+        return res
+            
+
+
+
+
